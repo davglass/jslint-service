16c093648a4471acd6f83febdbaf5fe3c8bc45f0
Dav Glass <davglass@gmail.com>
2008-12-15 18:53:06 -0800

    Modified jslint-console to output JSON for errors

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
 $str = '';
-#$java = `which java`;
+$java = `which java`;
 $java = 'java';
 $dir = realpath('./');
 $jslint = $dir.'/lib/jslint/jslint-console.js';
-//$jslint = $dir.'/lib/jslint/fulljslint.js';
+$fulljslint = $dir.'/lib/jslint/fulljslint.js';
 $rhino = $dir.'/lib/rhino/js.jar';
 
 
@@ -14,27 +14,30 @@ $json = new stdclass();
 if ($_POST['source']) {
     $str = $_POST['source'];
     $tempName = tempnam(sys_get_temp_dir(), 'jslint-');
+    $tempNameOut = tempnam(sys_get_temp_dir(), 'jslint-');
     file_put_contents($tempName, stripslashes($str));
     chmod($tempName, 0755);
 }
 
 
 if ($tempName) {
-    $cmd = $java.' -jar '.escapeshellarg($rhino).' '.escapeshellarg($jslint).' '.escapeshellarg($tempName);
-    echo($cmd);
-    //$out = exec($cmd, $data);
+    $cmd = $java.' -jar '.escapeshellarg($rhino).' '.escapeshellarg($jslint).' '.$fulljslint.' '.escapeshellarg($tempName).' 2>&1';
+    //echo($cmd);
     $out = exec($cmd, $data);
-    $fileE = file_get_contents("php://stdin", "r");
-    echo $fileE;
     
+    $error = file_get_contents($tempNameOut);
+    $error = str_replace('js: ', '', $error);
+    $error = str_replace('"'.$tempName.'", ', '', $error);
 
-    echo('<pre>'.print_r($out, 1).'</pre>');
-    echo('<pre>'.print_r($data, 1).'</pre>');
+    //echo('<pre>'.$error.'</pre>');
+    echo($out);
+    //echo('<pre>'.print_r($data, 1).'</pre>');
+
 } else {
     $json->error = new stdclass();
     $json->error->message = 'No javascript to lint.';
+    echo(json_encode($json));
 }
 
 
-echo(json_encode($json));
 ?>
