4b309a0472ac02e1a6a4ff1fc3f3b33f0ba678dd
Dav Glass <davglass@gmail.com>
2008-12-15 17:30:40 -0800

    Added tests and README

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$str = '';
+$java = `which java`;
+$dir = realpath('./');
+$jslint = $dir.'/lib/jslint/jslint-console.js';
+$rhino = $dir.'/lib/rhino/js.jar';
+
+
+$json = new stdclass();
+
+if ($_POST['source']) {
+    $str = $_POST['source'];
+    $tempName = tempnam(sys_get_temp_dir(), 'jslint-');
+    file_put_contents($tempName, stripslashes($str));
+    chmod($tempName, 0755);
+}
+
+
+if ($tempName) {
+    $cmd = $java.' -jar '.escapeshellarg($rhino).' '.escapeshellarg($jslint).' '.escapeshellarg($tempName);
+    echo($cmd);
+    $out = exec($cmd, $data);
+
+    echo('<pre>'.print_r($out, 1).'</pre>');
+    echo('<pre>'.print_r($data, 1).'</pre>');
+} else {
+    $json->error = new stdclass();
+    $json->error->message = 'No javascript to lint.';
+}
+
+
+echo(json_encode($json));
+?>
