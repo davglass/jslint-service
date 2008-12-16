4784abf5f651531b501d9d369087579ed840650c
Dav Glass <davglass@gmail.com>
2008-12-15 17:55:25 -0800

    java testing

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if ($tempName) {
     $cmd = $java.' -jar '.escapeshellarg($rhino).' '.escapeshellarg($jslint).' '.escapeshellarg($tempName);
     echo($cmd);
     //$out = exec($cmd, $data);
-    $out = system($cmd, $data);
+    $out = exec($cmd, $data);
 
     echo('<pre>'.print_r($out, 1).'</pre>');
     echo('<pre>'.print_r($data, 1).'</pre>');
