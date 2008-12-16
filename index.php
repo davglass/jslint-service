a10fdfbf184ce11e32ff4711e725f9bff632aa45
Dav Glass <davglass@gmail.com>
2008-12-15 17:49:45 -0800

    java testing

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $str = '';
 $java = `which java`;
 $dir = realpath('./');
 $jslint = $dir.'/lib/jslint/jslint-console.js';
+$fulljslint = $dir.'/lib/jslint/fulljslint.js';
 $rhino = $dir.'/lib/rhino/js.jar';
 
 
@@ -18,7 +19,7 @@ if ($_POST['source']) {
 
 
 if ($tempName) {
-    $cmd = $java.' -jar '.escapeshellarg($rhino).' '.escapeshellarg($jslint).' '.escapeshellarg($tempName);
+    $cmd = $java.' -jar '.escapeshellarg($rhino).' '.escapeshellarg($jslint).' '.$fulljslint.' '.escapeshellarg($tempName);
     echo($cmd);
     $out = exec($cmd, $data);
 
